6bcbcab1fcaa93e72e04994fc86fb99150ca03d8
datastory.cz <honza@datastory.cz>
2019-11-25 19:27:11 +0100

    beta appky

--- js/script.js
+++ js/script.js
@@ -1,10 +1,81 @@
 ﻿import "./byeie"; // loučíme se s IE
+import { cams } from "./cams"
 
-/*
-// snadné načtení souboru pro každého!
-fetch("https://blabla.cz/blabla.json")
-  .then(response => response.json()) // nebo .text(), když to není json
-  .then(data => {
-    // tady jde provést s daty cokoliv
-  });
-*/
+Loader.load()
+
+let map = L.map('mapa')
+map.fitBounds([
+  [50.180416, 14.709320], //phorni
+  [49.939731, 14.218369], //lspodni 
+])
+
+L.tileLayer('https://mapserver.mapy.cz/turist-m/{z}-{x}-{y}', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">Mapy.cz, a.s.</a>, &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+const sezLogo = L.control({position: 'bottomleft'});
+sezLogo.onAdd = function(map){
+    var div = L.DomUtil.create('div', 'sez-logo');
+    div.innerHTML= '<a target="_blank" href="https://mapy.cz"><img src="https://api.mapy.cz/img/api/logo.svg"/>';
+    return div;
+}
+sezLogo.addTo(map);
+let routeCams = L.featureGroup().addTo(map) // prázná vrstva na vložení trasy a kamer
+
+let startEnd
+
+function flipCoords(coords) {
+  return coords.map(v => [v[1], v[0]])
+}
+
+function foundRoute(route) {
+  let seenCams = []
+  let trackCoords = []
+  route.getResults().geometry.forEach(coord => {
+    trackCoords.push([coord.x, coord.y])
+  })
+  const track = turf.lineString(trackCoords)
+  cams.forEach(cam => {
+    const isect = turf.lineIntersect(track, turf.lineString(cam))
+    if (isect.features.length > 0) {
+      seenCams.push(cam)
+    }
+  })
+  document.getElementById('info').innerHTML = `Po cestě tě uvidí ${seenCams.length} kamer!`
+  routeCams.clearLayers() // úklid mapy po předchozím hledání
+  // trasu do mapy
+  L.polyline(flipCoords(trackCoords), {color: 'red'}).addTo(routeCams)
+  // kamery do mapy
+  seenCams.forEach(cam => {
+    L.polygon(flipCoords(cam), {color: 'blue'}).addTo(routeCams)
+  })
+  // zoom na vysledek
+  map.fitBounds(routeCams.getBounds())
+}
+
+function gcode(query) {
+  new SMap.Geocoder(query + ', Praha', gcodeResp)
+}
+
+function gcodeResp(geocoder) {
+  if (!geocoder.getResults()[0].results.length) {
+      console.log('Neznama adresa')
+      return;
+  }
+  const res = geocoder.getResults()[0].results[0].coords
+  startEnd.push(SMap.Coords.fromWGS84(res.x, res.y))
+  if (startEnd.length == 2) {
+    new SMap.Route(startEnd, foundRoute, {criterion: 'turist2'})  // krátká pěší trasa
+  }
+}
+
+const form = document.getElementById("geocoder")
+form.onsubmit = function submitForm(event) {
+  event.preventDefault()
+  startEnd = []
+  const fromAddr = document.getElementById('gcode-from').value
+  const toAddr = document.getElementById('gcode-to').value
+  
+  gcode(fromAddr)
+  gcode(toAddr)
+}
---
+++ tools/reduce_geojson.py
@@ -0,0 +1,24 @@
+#%%
+import json
+
+# %%
+with open('./scratch/kamery.json', 'r', encoding='utf-8') as f:
+    d = json.loads(f.read())
+
+#%%
+def simpl(arr):
+    out = []
+    for pair in arr[0]:
+        out.append([
+            round(pair[0], 5), # https://gis.stackexchange.com/questions/8650/measuring-accuracy-of-latitude-and-longitude
+            round(pair[1], 5),
+        ])
+    return out
+# %%
+out = []
+for geom in d['geometries']:
+    out.append(simpl(geom['coordinates']))
+
+# %%
+with open('./js/cams.js', 'w', encoding='utf-8') as f:
+    f.write('export const cams = ' + json.dumps(out, ensure_ascii=False))
